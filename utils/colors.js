30ea6612c1d9e2e72da0dc330dc5ee0620cd968f
paulocsmg <paulocsmg@gmail.com>
2019-04-09 18:06:11 -0300

    App colors
    Define app colors

---
+++ utils/colors.js
@@ -0,0 +1,8 @@
+export const primary = '#00796B';
+export const lightPrimary = '#80CBC4';
+export const secondary = '#512da8';
+export const lightSecondary = '#9575cd';
+export const black = '#212121';
+export const gray = '#757575';
+export const lightGray = '#BDBDBD';
+export const white = '#FFFFFF';
